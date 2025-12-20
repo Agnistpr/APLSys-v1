f57bafb9d1e8b9978d52d90a0b05ca0e3cb1ea14
GreggMays <108717496+GreggMays@users.noreply.github.com>
2025-12-20 17:17:42 +0800

    .

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,5 @@
 // Frontend config used by files inside `src/`
-export const API_BASE_URL =(() => {
-  return "https://aplsys-backend-331525477255.asia-east1.run.app";
-})();
-
+export const API_BASE_URL = "https://aplsys-backend-331525477255.asia-east1.run.app";
 
 export const DEV_TEST_URL = "http://localhost:8080";
 
